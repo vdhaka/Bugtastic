68a8910af31ed436f42d57aa6355dcdc319f9f10
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-09-21 11:17:29 -0700

    Fix variable name to snake case

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ int main()
 {
   int age;
   string name;
-  char last initial;
+  char last_initial;
 
   cout<<"Hi. What is your first name? "
   cin<<name;
   cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
+  cin>>last_initial;
+  cout<<"Thanks, "<<name<< <<last_initial<<.<<endl;
   cout<<"Please also tell me how old you are: ";
   cin>>age>>endl;
 
